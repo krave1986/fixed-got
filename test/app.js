e3e921f7294902a5d89b5d1f1e27bf36ac5240a3
Krave <krave@qq.com>
2023-01-12 07:20:53 +0000

    ready to publish to npm

--- test/app.js
+++ test/app.js
@@ -2,8 +2,6 @@
 
 const fixedGot = require("../index")
 
-console.log(fixedGot);
-
 async function goToGet() {
     const result = await fixedGot('http://localhost:3000', 1000).json()
     console.log(result);
